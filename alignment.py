30c2888756991122c7236a7c500120a5a6c8c107
haijohn <haijohnzju@gmail.com>
2015-01-11 11:23:54 +0800

    affine gap panelty

--- alignment.py
+++ alignment.py
@@ -7,6 +7,7 @@ Unit5: How Do We Compare Sequence ?
 """
 
 from utils import *
+from graph import *
 import sys
 sys.setrecursionlimit(10000)
 
@@ -108,10 +109,14 @@ def manhatten_tourist(n, m, down_matrix, right_matrix):
 # end Manhattan tourist problem
 
 
-def toplogical_ordering(graph):
-    ordering = []
+def toplogical_ordering(graph, src, dest):
+    ordering = [src]
+    for edge in graph.out_edges(src):
+        graph.remove_edge(edge)
+    graph.remove_node(src)
     candidates = [node for node in graph.get_nodes() 
-                          if graph.indegree(node) == 0]
+                          if graph.indegree(node) == 0 
+                             and node != dest ]
     while candidates:
         candidate = candidates.pop()
         ordering.append(candidate)
@@ -119,9 +124,29 @@ def toplogical_ordering(graph):
             graph.remove_edge(edge)
         graph.remove_node(candidate)
         candidates = [node for node in graph.get_nodes() 
-                          if graph.indegree(node) == 0]
+                          if graph.indegree(node) == 0
+                              and node != dest]
+    ordering.append(dest)
     return ordering
 
+
+def bulid_graph(graph_file):
+    g = DiGraph()
+    with open(graph_file) as f:
+        source = f.readline().strip()
+        sink = f.readline().strip()
+        for line in f:
+            src,dest = line.strip().split('->')
+            dest,weight = dest.split(':')
+            src = Node(src)
+            dest = Node(dest)
+            edge = WeightedEdge(src,dest,weight)
+            if not g.has_node(src):
+                g.add_node(src)
+            if not g.has_node(dest):
+                g.add_node(dest)
+            g.add_edge(edge)
+    return Node(source), Node(sink), g
 
 # begin longest common substring problem
 def lcs_backtrack(v, w):
@@ -305,8 +330,8 @@ def print_alignment(backtrack, v, w, i, j, s=None, p=None):
         p.append(w[j-1])
     else:
         print_alignment(backtrack,v,w,i-1,j-1,s,p)
-	s.append(v[i-1])
-	p.append(w[j-1])
+        s.append(v[i-1])
+        p.append(w[j-1])
     return s,p
 
 
@@ -327,8 +352,8 @@ def print_local_alignment(backtrack,v, w, i, j, s=None, p=None):
         p.append(w[j-1])
     else:
         print_local_alignment(backtrack,v,w,i-1,j-1,s,p)
-	s.append(v[i-1])
-	p.append(w[j-1])
+        s.append(v[i-1])
+        p.append(w[j-1])
     return s,p
 
 
@@ -376,9 +401,8 @@ def edit_distance(v,w):
                                )
     return s[m,n]
 
-def fitting_alignment(v,w,gap_penalty=-1):
-    m = len(v)
-    n = len(w)
+def fitting_alignment(v, w, gap_penalty=-1):
+    m,n = len(v),len(w)
     s = {}
     max_score = 0
     max_position = (0,0)
@@ -432,8 +456,8 @@ def print_fitting_alignment(backtrack,v, w, i, j, s=None, p=None):
         p.append(w[j-1])
     else:
         print_fitting_alignment(backtrack,v,w,i-1,j-1,s,p)
-	s.append(v[i-1])
-	p.append(w[j-1])
+        s.append(v[i-1])
+        p.append(w[j-1])
     return s,p
 
 def solve_fitting_alignment():
@@ -454,20 +478,123 @@ def solve_fitting_alignment():
     print ''.join(s)
     print ''.join(p)
 
-c = read_content('1.txt')
-v = c[0]
-w = c[1]
-#v = 'PAWHEAE'
-#w = 'HEAGAWGHEE'
-lw = len(w)
-b,scores = fitting_alignment(v,w,gap_penalty=-2)
-#print scores
-margins = [item for item in scores.items() if item[0][0] == len(v)]
-max_p = reduce(lambda x,y: y if x[1] < y[1] else x,margins)
-#print max_p
-(i,j),score = max_p
-s,p = print_fitting_alignment(b, v, w, i, j)
-print score
-print ''.join(s)
-print ''.join(p)
+
+def solve_overlap_alignment():
+    c = read_content('1.txt')
+    v = c[0]
+    w = c[1]
+    #v = 'PAWHEAE'
+    #w = 'HEAGAWGHEE'
+    lw = len(w)
+    b,scores = fitting_alignment(v,w,gap_penalty=-2)
+    #print scores
+    margins = [item for item in scores.items() if item[0][0] == len(v)]
+    max_p = reduce(lambda x,y: y if x[1] < y[1] else x,margins)
+    #print max_p
+    (i,j),score = max_p
+    s,p = print_fitting_alignment(b, v, w, i, j)
+    print score
+    print ''.join(s)
+    print ''.join(p)
+
+
+def affine_gap_panelty(v, w, score_matrix, gap_penalty=-5, extension_panelty=-1):
+    """ 3 level manhatten """
+    m,n = len(v),len(w)
+    sl = {} #lower score
+    sm = {} #middle score
+    su = {} #up score
+    sl[(0,0)] = len(w)*gap_penalty
+    sm[(0,0)] = 0
+    su[(0,0)] = len(w)*gap_penalty
+    backtrack_l = {}
+    backtrack_m = {}
+    backtrack_u = {}
+    for i in range(1, m+1):
+        su[(i,0)] = len(w)*gap_penalty
+        sl[(i,0)] = gap_penalty + (i-1)*extension_panelty
+        sm[(i,0)] = sl[(i,0)]
+        if i == 1:
+            backtrack_l[(i,0)] = "open" 
+        else:
+            backtrack_l[(i,0)] = "extention"
+        backtrack_m[(i,0)] = "down close"
+    for j in range(1, n+1):
+        sl[(0,j)] = len(w)*gap_penalty
+        su[(0,j)] = gap_penalty + (i-1)*extension_panelty   
+        sm[(0,j)] = su[(0,j)]
+        if j == 1:
+            backtrack_u[(0,j)] = "open" 
+        else:
+            backtrack_u[(0,j)] = "extention"
+        backtrack_m[(0,j)] = "close"
+    for i in range(1,m+1):
+        for j in range(1,n+1):
+            sl[(i,j)] = max(
+                          sl[(i-1,j)] + extension_panelty,
+                          sm[(i-1,j)] + gap_penalty
+                         )
+            if sl[(i,j)] == sl[(i-1,j)] + extension_panelty:
+                backtrack_l[(i,j)] = 'extention'
+            else:
+                backtrack_l[(i,j)] = 'open'
+            su[(i,j)] = max(
+                          su[(i,j-1)] + extension_panelty,
+                          sm[(i,j-1)] + gap_penalty
+                         )
+            if su[(i,j)] == su[(i,j-1)] + extension_panelty:
+                backtrack_u[(i,j)] = 'extention'
+            else:
+                backtrack_u[(i,j)] = 'open'
+            sm[(i,j)] = max(
+                          sm[(i-1,j-1)] + score_matrix[(v[i-1],w[j-1])],
+                          sl[(i,j)],
+                          su[(i,j)]
+                         )
+            if sm[(i,j)] == sm[(i-1,j-1)] + score_matrix[(v[i-1],w[j-1])]:
+                backtrack_m[(i,j)] = 'diag'
+            elif sm[(i,j)] == sl[(i,j)]:
+                backtrack_m[(i,j)] = 'down close'
+            elif sm[(i,j)] == su[(i,j)]:
+                backtrack_m[(i,j)] = 'up close'
+    return sm[(m,n)], backtrack_m, backtrack_l, backtrack_u
+
+def print_affine_gap_panelty(backtrack_m, backtrack_l, backtrack_u, v, w, i, j, state='m', s=None, p=None):
+    if i==0 and j==0:
+        return
+    if s is None:
+        s = []
+    if p is None:
+        p = []
+    if state == 'm':
+        if backtrack_m[(i,j)] == 'diag':
+            print_affine_gap_panelty(backtrack_m, backtrack_l, backtrack_u, v, w, i-1, j-1, 'm', s, p)
+            s.append(v[i-1])
+            p.append(w[j-1])
+        elif backtrack_m[(i,j)] == 'up close':
+            print_affine_gap_panelty(backtrack_m, backtrack_l, backtrack_u, v, w, i, j, 'u', s, p)
+        elif backtrack_m[(i,j)] == 'down close':
+            print_affine_gap_panelty(backtrack_m, backtrack_l, backtrack_u, v, w, i, j, 'l', s, p)
+    elif state == 'u':
+        if backtrack_u[(i,j)] == 'open':
+            print_affine_gap_panelty(backtrack_m, backtrack_l, backtrack_u, v, w, i, j-1, 'm', s, p)
+        elif backtrack_u[(i,j)] == 'extention':
+            print_affine_gap_panelty(backtrack_m, backtrack_l, backtrack_u, v, w, i, j-1, 'u', s, p)
+        s.append('-')
+        p.append(w[j-1])
+    elif state == 'l':
+        if backtrack_l[(i,j)] == 'open':
+            print_affine_gap_panelty(backtrack_m, backtrack_l, backtrack_u, v, w, i-1, j, 'm', s, p)
+        elif backtrack_u[(i,j)] == 'extention':
+            print_affine_gap_panelty(backtrack_m, backtrack_l, backtrack_u, v, w, i-1, j, 'l', s, p)
+        s.append(v[i-1])
+        p.append('-')
+    return s,p        
+
+
+
+
+
+
+
 
